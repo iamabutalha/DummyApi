a1dde65261b17434664ab5514f3c391f4dccbc6e
iamabutalha <maabutalhakhalil@gmail.com>
2025-05-18 14:16:54 +0500

    fix issue in api

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import { products } from "./products/product.js";
+import { users } from "./products/users.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:keyword", (req, res) => {
+  const { keyword } = req.params;
+
+  const findProduct = products.filter((product) =>
+    product.keywords.includes(keyword.toLowerCase())
+  );
+  console.log(findProduct);
+
+  if (!findProduct) {
+    return res.json("No product found for this Keyword");
+  }
+
+  return res.json(findProduct);
+});
+
+app.get("/products/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  const userProducts = products.filter((product) => product.userid == id);
+  console.log(userProducts);
+
+  if (!userProducts) {
+    return res.json("User not found with this id");
+  }
+
+  return res.json(userProducts);
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const findUser = users.find((user) => user.id === Number(id));
+
+  if (!findUser) {
+    return res.json("user Not found");
+  }
+  res.json(findUser);
+});
+
+app.listen(3000, () => {
+  console.log("App running on PORT 3000");
+});
---
+++ products/users.js
@@ -0,0 +1,62 @@
+export const users = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    occupation: "Software Engineer",
+    phone: "555-123-4567",
+  },
+  {
+    id: 2,
+    name: "Michael Smith",
+    occupation: "Graphic Designer",
+    phone: "555-234-5678",
+  },
+  {
+    id: 3,
+    name: "Jessica Lee",
+    occupation: "Marketing Specialist",
+    phone: "555-345-6789",
+  },
+  {
+    id: 4,
+    name: "David Kim",
+    occupation: "Data Analyst",
+    phone: "555-456-7890",
+  },
+  {
+    id: 5,
+    name: "Abu Talha Khalil",
+    occupation: "Product Manager",
+    phone: "555-567-8901",
+  },
+  {
+    id: 6,
+    name: "James Brown",
+    occupation: "UX Designer",
+    phone: "555-678-9012",
+  },
+  {
+    id: 7,
+    name: "Olivia Martinez",
+    occupation: "HR Coordinator",
+    phone: "555-789-0123",
+  },
+  {
+    id: 8,
+    name: "Daniel Wilson",
+    occupation: "Accountant",
+    phone: "555-890-1234",
+  },
+  {
+    id: 9,
+    name: "Sophia Clark",
+    occupation: "Legal Assistant",
+    phone: "555-901-2345",
+  },
+  {
+    id: 10,
+    name: "Liam Thompson",
+    occupation: "Sales Executive",
+    phone: "555-012-3456",
+  },
+];
